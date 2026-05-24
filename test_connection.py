68b723c6a3b94714eb362b54be523fd4497c9c25
Funsky211 <168831597+Funsky211@users.noreply.github.com>
2026-05-24 13:26:52 +0200

    test python script

---
+++ test_connection.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from alpaca.trading.client import TradingClient
+
+load_dotenv()
+
+client = TradingClient(
+    os.getenv("ALPACA_API_KEY"),
+    os.getenv("ALPACA_SECRET_KEY"),
+    paper=True,
+)
+
+account = client.get_account()
+print(f"Connecté ! Cash disponible : ${float(account.cash):,.2f}")
